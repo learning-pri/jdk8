00aef4409a2a2259f6b0a25a5a0ddf4c703c970d
priyankshukla <priyank.shukla@eko.co.in>
2019-06-05 00:00:50 +0530

    Implementing the Lambda expression with functional interface

--- src/com/pri/jdk8/lambda/LambdaExample.java
+++ src/com/pri/jdk8/lambda/LambdaExample.java
@@ -2,9 +2,23 @@ package com.pri.jdk8.lambda;
 
 public class LambdaExample {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public void greet(Greeting greeting) {
+		greeting.perform();
+	}
 
+	public static void main(String[] args) {
+
+		Greeting greetingLambda = () -> System.out.println("Greeting...");
+		Greeting innerClassGreeting = new Greeting() {
+			public void perform() {
+				System.out.println("Hello World!");
+			}
+
+		};
+
+		LambdaExample greet = new LambdaExample();
+		greet.greet(greetingLambda);
+		greet.greet(innerClassGreeting);
 	}
 
 }
