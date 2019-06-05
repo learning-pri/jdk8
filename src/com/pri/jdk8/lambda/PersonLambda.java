c9160f830213ab3ca7764aaefba5b7c0ff7d54c5
priyankshukla <priyank.shukla@eko.co.in>
2019-06-05 09:11:57 +0530

    Testing Lambda expression with functional interface

---
+++ src/com/pri/jdk8/lambda/PersonLambda.java
@@ -0,0 +1,41 @@
+package com.pri.jdk8.lambda;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class PersonLambda {
+
+	public static void main(String[] args) {
+
+		PersonLambda personLambda = new PersonLambda();
+		List<Person> people = Arrays.asList(
+				new Person("Priyank", "Shukla", 35),
+				new Person("Ruchi", "Shukla", 35),
+				new Person("Nishant", "Bajpai", 28),
+				new Person("Aditya", "Tyagi", 37)
+				);
+		
+		
+		personLambda.performConditional(people, p -> true);
+		
+		Collections.sort(people, (p1, p2) -> p1.getLastName().compareTo(p2.getLastName()));
+System.out.println("###################  PRINTING LIST POST SHORTING  #################");
+		personLambda.performConditional(people, p ->true);
+		
+		
+		System.out.println("##################################################");
+		personLambda.performConditional(people, p ->p.getLastName().startsWith("S"));
+	}
+	
+	public void performConditional(List<Person> p, Condition c) {
+		for (Person person : p) {
+			if(c.test(person))
+			System.out.println(person);
+		}
+	}
+
+}
+interface Condition{
+	public boolean test(Person p);
+}
